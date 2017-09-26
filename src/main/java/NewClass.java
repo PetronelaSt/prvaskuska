c268e1e34d17a46532356e747378bfc66def1491
Oleksandr <o.yuryk1324@gmail.com>
2017-09-26 18:13:08 +0200

    Novy vypis

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -11,5 +11,6 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Ahoj, prišlo ti to?");
+        System.out.println("Ahoj, ano, a tebe?");
     }
 }
