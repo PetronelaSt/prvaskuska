2d0d428725a298bdc6e7abd200a95c4e2332cd45
Petronela <Petronela@LAPTOP-T4LE86T0.local>
2017-09-26 19:15:42 +0200

    Odpoveď

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -12,5 +12,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Ahoj, prišlo ti to?");
         System.out.println("Ahoj, ano, a tebe?");
+        System.out.println("Jop, už ani fb nepotrebujeme =D");
     }
 }
